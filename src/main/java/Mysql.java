dad636ff17eb88967efb207bfc415d1774a32146
Viktor Hundahl Strate <viktorstrate@gmail.com>
2015-02-20 17:55:01 +0100

    Filled in the functions

--- src/main/java/Mysql.java
+++ src/main/java/Mysql.java
@@ -1,3 +1,5 @@
+import java.sql.*;
+
 /**
  * Created by viktorstrate on 20/02/15.
  */
@@ -7,8 +9,12 @@ public class Mysql {
     private String password;
     private String dbName;
 
+    private Connection connection;
+    private Statement statement;
+
     public Mysql(){
         try {
+            // This loads the driver
             Class.forName("com.mysql.jdbc.Driver");
         } catch (ClassNotFoundException e) {
             e.printStackTrace();
@@ -21,27 +27,45 @@ public class Mysql {
      * @param username the username of the mysql server.
      * @param password the password of the mysql server. Use an empty string for no password.
      */
-    public Mysql(String host, String username, String password){
+    public Mysql(String host, String username, String password, String databaseName){
         this();
-        this.host = host;
-        this.username = username;
-        this.password = password;
+        this.connect(host, username, password, databaseName);
+
     }
 
     /**
-     * Select which database to connect to.
-     * @param databaseName the name of the database, to connect to.
+     * If not connected when initialized.
+     * @param host the hostname of the mysql server, fx. "localhost" or "example.com"
+     * @param username the username of the mysql server.
+     * @param password the password of the mysql server. Use an empty string for no password.
      */
-    public void connectToDB(String databaseName){
-        this.dbName = databaseName;
+    public void connect(String host, String username, String password, String databaseName){
+        this.host = host;
+        this.username = username;
+        this.password = password;
+        try {
+            // Setup the connection with the db
+            connection = DriverManager.getConnection("jdbc:mysql://"+host+"/"+databaseName+"?"
+            + "user="+username+"&password="+password);
+            // Statements allow to issue SQL queries to the database
+            statement = connection.createStatement();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
-
     /**
      * Executes a query, to the mysql server
      * @param query the query to execute fx. "SELECT * FROM table"
      * @return the selected rows, if any else returning NULL
      */
-    public Object query(String query){
+    public ResultSet query(String query){
+
+        try {
+            return statement.executeQuery(query);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
         return null;
     }
 
